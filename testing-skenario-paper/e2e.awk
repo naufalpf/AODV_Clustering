a40f9022bdba9d0b77c990c69b97c4647950e79f
Unknown <naufalpf@gmail.com>
2018-12-14 20:36:11 +0700

    skenario

---
+++ testing-skenario-paper/e2e.awk
@@ -0,0 +1,61 @@
+# ===================================================================
+# AWK Script for calculating: 
+#     => Average End-to-End Delay.
+# ===================================================================
+
+BEGIN {
+  seqno = -1;    
+#    droppedPackets = 0; 
+#    receivedPackets = 0; 
+  count = 0;
+}
+
+{
+  if($4 == "AGT" && $1 == "s" && seqno < $6) {
+    seqno = $6;
+  } 
+#	else if(($4 == "AGT") && ($1 == "r")) {
+#            receivedPackets++;
+#    } else if ($1 == "D" && $7 == "tcp" && $8 > 512){
+#            droppedPackets++;            
+#  } 
+
+  #end-to-end delay
+  if($4 == "AGT" && $1 == "s") {
+    start_time[$6] = $2;
+  } else if(($7 == "cbr") && ($1 == "r")) {
+    end_time[$6] = $2;
+  } else if($1 == "D" && $7 == "cbr") {
+    end_time[$6] = -1;
+  } 
+}
+
+
+END {        
+  for(i=0; i<=seqno; i++) {
+    if(end_time[i] > 0) {
+      delay[i] = end_time[i] - start_time[i];
+      count++;
+    }
+    else {
+      delay[i] = -1;
+    }
+  }
+
+  for(i=0; i<=seqno; i++) {
+    if(delay[i] > 0) {
+      n_to_n_delay = n_to_n_delay + delay[i];
+    }         
+  }
+
+  # n_to_n_delay = n_to_n_delay/count;
+
+  print "\n";
+
+  print "GeneratedPackets \t= " seqno+1;
+  print "ReceivedPackets  \t= " receivedPackets;
+  print "Packet Delivery Ratio \t= " receivedPackets/(seqno+1)*100 "%";
+  print "Total Dropped Packets \t= " droppedPackets;
+  print "Average End-to-End Delay \t= " n_to_n_delay * 1000 " ms";
+  print "\n";
+} 
---
+++ testing-skenario-paper/setdes500x500.tcl
@@ -0,0 +1,77 @@
+#
+# nodes: 15, speed type: 1, min speed: 10.00, max speed: 50.00
+# avg speed: 22.69, pause type: 1, pause: 1.00, max x: 500.00, max y: 500.00
+#
+# source: node 0
+$node_(0) set X_ 0.13997455992
+$node_(0) set Y_ 2000.13997455992
+$node_(0) set Z_ 0.000000000000
+
+$node_(1) set X_ 300.413997455992
+$node_(1) set Y_ 1800.413997455992
+$node_(1) set Z_ 0.000000000000
+
+$node_(2) set X_ 300.903611406673
+$node_(2) set Y_ 1600.903611406673
+$node_(2) set Z_ 0.000000000000
+
+$node_(3) set X_ 400.903611406673
+$node_(3) set Y_ 1400.903611406673
+$node_(3) set Z_ 0.000000000000
+
+$node_(4) set X_ 600.903611406673
+$node_(4) set Y_ 1300.903611406673
+$node_(4) set Z_ 0.000000000000
+
+$node_(5) set X_ 900.903611406673
+$node_(5) set Y_ 1200.903611406673
+$node_(5) set Z_ 0.000000000000
+
+$node_(6) set X_ 800.903611406673
+$node_(6) set Y_ 1300.903611406673
+$node_(6) set Z_ 0.000000000000
+
+$node_(7) set X_ 900.903611406673
+$node_(7) set Y_ 1500.903611406673
+$node_(7) set Z_ 0.000000000000
+
+$node_(8) set X_ 900.413997455992
+$node_(8) set Y_ 1900.413997455992
+$node_(8) set Z_ 0.000000000000
+
+$node_(9) set X_ 900.413997455992
+$node_(9) set Y_ 1800.413997455992
+$node_(9) set Z_ 0.000000000000
+
+$node_(10) set X_ 500.413997455992
+$node_(10) set Y_ 1900.413997455992
+$node_(10) set Z_ 0.000000000000
+
+$node_(11) set X_ 300.413997455992
+$node_(11) set Y_ 1300.413997455992
+$node_(11) set Z_ 0.000000000000
+
+$node_(12) set X_ 800.903611406673
+$node_(12) set Y_ 1100.903611406673
+$node_(12) set Z_ 0.000000000000
+
+$node_(13) set X_ 1100.903611406673
+$node_(13) set Y_ 1300.903611406673
+$node_(13) set Z_ 0.000000000000
+
+# Destination NOde : 14
+$node_(14) set X_ 1100.903611406673
+$node_(14) set Y_ 1100.903611406673
+$node_(14) set Z_ 0.000000000000
+#
+# Destination Unreachables: 7
+#
+# Route Changes: 12
+#
+# Link Changes: 7
+#
+# Node | Route Changes | Link Changes
+#    0 |             8 |            4
+#    1 |             7 |            4
+#    2 |             9 |            6
+#
